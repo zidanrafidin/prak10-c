a14cbaac2c32a887437ba7391b1f720bff6d8861
zidan <zidanrafidin@gmail.com>
2025-09-29 10:01:47 +0700

    add no3

--- no3_10.c
+++ no3_10.c
@@ -7,5 +7,43 @@
 
 #include <stdio.h>
 int main(){
-    int 
+    int bilangan, total = 0, start = 1;
+    char ulang;
+    int max = 0;
+    int min = 999999999;
+
+    while(1){
+        printf("Masukkan bilangan ke-%d : ", start);
+        scanf("%d", &bilangan);
+        total += bilangan;
+        start++;
+
+        if(bilangan > max){
+            max = bilangan;
+        }
+        if(bilangan < min){
+            min = bilangan;
+        }
+
+        printf("Mau memasukkan data lagi [y/t] ? ");
+        scanf(" %c", &ulang);
+
+        if(ulang == 't' || ulang == 'T'){
+            printf("Total bilangan = %d\n", total);
+            break;
+        } else if (ulang =='y' || ulang == 'Y'){
+            continue;
+        } else {
+            printf("Input tidak valid. Program dihentikan.\n");
+            continue;
+        }
+    }
+    printf("Total bilangan = %d\n", total);
+    float rata = total / (start - 1);
+    printf("Rata-rata : %.2f\n", rata);
+
+    printf("Maksimum : %d\n", max);
+    printf("Minimum : %d\n",min);
+
+    return 0;
 }
