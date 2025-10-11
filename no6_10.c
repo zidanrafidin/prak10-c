168d426733f40fb9f6bbd623797776d052ffb212
zidan <zidanrafidin@gmail.com>
2025-10-11 20:26:20 +0700

    revisi

--- no6_10.c
+++ no6_10.c
@@ -1,8 +1,3 @@
-//Buatlah program untuk menghitung jumlah angka dari suatu bilangan.
-//Contohnya :
-//Jumlah angka dari bilangan 3255 = 3 + 2 + 5 + 5 = 15
-// Jumlah angka dari bilangan 4589 = 4 + 5 + 8 + 9 = 26
-// dan sebagainya.
 #include <stdio.h>
 
 int main() {
@@ -11,12 +6,19 @@ int main() {
     printf("Masukkan bilangan: ");
     scanf("%d", &n);
 
+    printf("Jumlah angka dari bilangan %d = ", n);
     while (n != 0) {
         digit = n % 10;   // ambil digit terakhir
         sum += digit;     // tambahkan ke jumlah
         n = n / 10;       // buang digit terakhir
-    }
 
-    printf("Jumlah angka = %d\n", sum);
+        if(n!=0){
+            printf("%d + ",digit);
+        } else {
+            printf("%d",digit);
+        }
+    }
+    printf(" = %d\n", sum);
+
     return 0;
 }
